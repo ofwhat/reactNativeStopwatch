da4ddcfb077bfe65d37d7c9d75ecbf45de3bbc16
ofwhat <ofwhat@gmail.com>
2016-10-25 00:14:12 +0900

    testing flexbox

--- index.android.js
+++ index.android.js
@@ -1,69 +1,72 @@
 import React, {Componant} from 'react';
-import {
-  Text,
-  View,
-  AppRegistry,
-  StyleSheet
-} from 'react-native';
+import {Text, View, AppRegistry, StyleSheet} from 'react-native';
 
-var StopWatch = React.createClass ({
-  render: function() {
-    return <View>
-      <View>
-        <View>
-          <Text>
-            00:00.00
-          </Text>
+var StopWatch = React.createClass({
+    render: function() {
+        return <View style={styles.container}>
+            <View style={[styles.header, this.border('yellow')]}>
+                <View style={[styles.timerWrapper, this.border('red')]}>
+                    <Text>
+                        00:00.00
+                    </Text>
+                </View>
+                <View style={[styles.buttonWrapper, this.border('green')]}>
+
+                    {this.startStopButton()}
+                    {this.lapButton()}
+
+                </View>
+            </View>
+
+            <View style={[styles.footer, this.border('blue')]}>
+                <Text>
+                    I am a list Laps
+                </Text>
+
+            </View>
+
         </View>
-        <View>
-
-        {this.startStopButton()}
-        {this.lapButton()}
-
+    },
+    startStopButton: function() {
+        return <View>
+            <Text>
+                Start
+            </Text>
         </View>
-      </View>
+    },
+    lapButton: function() {
+        return <View>
+            <Text>
+                Lap
+            </Text>
+        </View>
 
-      <View>
-        <Text>
-          I am a list Laps
-        </Text>
-
-      </View>
-
-    </View>
-},
-  startStopButton: function() {
-    return   <View>
-      <Text>
-        Start
-      </Text>
-    </View>
-  },
-  lapButton: function() {
-    return <View>
-    <Text>
-      Lap
-    </Text>
-  </View>
-
-}
+    },
+    border: function(color){
+      return{
+        borderColor: color,
+        borderWidth: 4
+      }
+    }
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'stretch'
-  },
-  header: {
-    flex: 1
-  },
-  footer: {
-    flex: 1
-  }
+    container: {
+        flex: 1,
+        alignItems: 'stretch'
+    },
+    header: {
+        flex: 1
+    },
+    footer: {
+        flex: 1
+    },
+    timerWrapper: {
+        flex: 1
+    },
+    buttonWrapper: {
+        flex: 9
+    }
 });
-
-
-
-
 
 AppRegistry.registerComponent('stopwatch', () => StopWatch);
